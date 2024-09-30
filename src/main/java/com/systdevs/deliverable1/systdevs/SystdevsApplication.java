c74ea32acdbb43090d803a636ec1f1d69da41f57
Connor Uhrig <connor@connoruhrig.com>
2024-09-30 12:39:07 -0400

    First Commit

---
+++ src/main/java/com/systdevs/deliverable1/systdevs/SystdevsApplication.java
@@ -0,0 +1,15 @@
+package com.systdevs.deliverable1.systdevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystdevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystdevsApplication.class, args);
+
+		
+	}
+
+}
---
+++ src/test/java/com/systdevs/deliverable1/systdevs/SystdevsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systdevs.deliverable1.systdevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystdevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
